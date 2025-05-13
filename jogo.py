8cc2446b937b8410a1034c604c79c0ddd31119b6
Legondi <leogdias12@gmail.com>
2025-05-13 20:31:09 -0300

    save

--- jogo.py
+++ jogo.py
@@ -43,26 +43,36 @@ def valkyrie():
     info_stats = f"Vida: {life}\nAtaque: {atack}\nDefesa: {defense}"
     return life, atack, defense, start_point, info_stats
 
+#apaga restos de info do console antes de iniciar 
 limparTELA()
 
+#variaveis pre definidas para usu global
 esco_persona = ""
 life, atack, defense, start_Point, info_Stats = 0, 0, 0, "", ""
 
 print("bem vindo! escolha seu personagem: ")
 
+#validação de escolha
 while esco_persona != "1" and esco_persona != "2":
 
  esco_persona = input("1- Mago\n2- Guerreira\n3- Informações de Personagens:")
 
+#estrutura para decidir o personagem (previne erros)
  if esco_persona == "1":
     limparTELA()
+    #carrega status do mago
     life, atack, defense, start_Point, info_Stats = mage()
  elif esco_persona == "2":
     limparTELA()
+    #carrega status da guerreira
     life, atack, defense, start_Point, info_Stats = valkyrie()
  elif esco_persona == "3":
     limparTELA()
+
+    #estrura para ver as informacoes e decidir qual personagem o uso tem mais familiaridade
     esco_info = ""
+
+    #estrutura para decidir qual o status do personagem quer ver (previne erros)
     while esco_info != "1" and esco_info != "2" and esco_info != "3":
         print("qual dos persongens voce quer ver historia e status:?")
         esco_info = input("1- Mago\n2- Guerreira\n3- voltar\n:")
@@ -70,25 +80,52 @@ while esco_persona != "1" and esco_persona != "2":
     
         if esco_info == "1":
             life, atack, defense, start_Point, info_Stats = mage() 
+            
+            #printa as infos com base na escolha
             print(info_Stats)
+            #pausa o codigo para a visualização das infos
             Exit = input("\naperte qualquer tecla para sair:")
+        
         elif esco_info == "2":
             life, atack, defense, start_Point, info_Stats = valkyrie() 
+            
+            #printa as infos com base na escolha
             print(info_Stats)
+            #pausa o codigo para a visualização das infos
             Exit = input("\naperte qualquer tecla para sair:")
+       
         elif esco_info == "3":
+            
+            #volta para o primeiro menu e limpa o console para uma melhor visu
             limparTELA()
         else:
+           
+           #impede o erro
            limparTELA()
            print("opcao invalida\n") 
 
     limparTELA()
  else:
+
+    #impede o erro
     limparTELA()
     print("persongam nao encontrado ;(\n")
 
 #passado a historia e contexto para o usu surge as opcoes!
 
+
+#                                    A
+#PONTO DE BAIXA ALTERAÇÃO DE CODIGO  |
+
+
+#_____________________________________________________________________________________________________________________________________
+
+
+#PONTO DE ALTA ALTERAÇÃO DE CODIGO  |
+#                                  V
+
+
+#define a variavel para usar de parametro na hora de dar respawn do personagem
 if start_Point == "V":
     print("Voce escolheu a Guerreira\n")
 else:
